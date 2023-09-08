f627c24d9611c94a12c2f517196e6a180d36cd71
moh1651 <mohamedabderrahmane980@gmail.com>
2023-09-08 02:28:03 -0700

    tetete

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is used to made known where the code start
+ *
+ * Return: help to know how it ends
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
+}
