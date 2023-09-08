d108f431b205ead0c153965e5a694c16badb8a65
moh1651 <mohamedabderrahmane980@gmail.com>
2023-09-08 03:33:27 -0700

    tetete

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- write anything about this program printing size of types of file
+ * Return: return always something nul
+ */
+
+int main(void)
+{
+	printf("the sz of a char:%lu byte", sizeof(char));
+	printf("the sz of a int: %lu byte", sizeof(int));
+	printf("the sz of a int: %lu byte", sizeof(long int));
+	printf("the sz of a int: %lu byte", sizeof(long long int));
+	printf("the sz of a int: %lu byte", sizeof(float));
+}
