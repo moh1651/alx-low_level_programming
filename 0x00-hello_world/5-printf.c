da0b452dd8ddbbd4606aea83f3d569973220e396
moh1651 <mohamedabderrahmane980@gmail.com>
2023-09-08 02:50:12 -0700

    tetete

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this function printf print without create empty line at the end
+ *
+ * Return: help to end the c file 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n";
+			return (0);
+			}
